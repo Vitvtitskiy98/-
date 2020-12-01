bd6cf3fad915c057ecea73b4116e6cc5875d200f
Vlad Vitvitskiy <vladyn19981998@gmail.com>
2020-12-01 12:58:31 +0300

    Добавьте файлы проекта.

---
+++ Практикум/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Практикум
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Удалите из сообщения только те русские слова, которые начинаются на гласную букву.");
+            var str = "текст, в котором нужно удалить слова, которые начинаются на гласную букву.";
+            var result = Regex.Replace(str, @"\b[аиоуеюяы][а-я]+", "");
+            Console.WriteLine(result);
+            Console.WriteLine();
+
+            Console.WriteLine("2.В сообщении может содержаться дата в формате дд.мм.гггг. " +
+                "В заданном формате дд - целое число из диапазона " +
+                "\nот 1 до 31, мм - целое число из диапазона от 1 до 12, а гггг - целое число из диапазона " +
+                "\nот 1900 до 2010(если какая - то часть формата нарушена, то данная подстрока в качестве " +
+                "\nдаты не рассматривается).Замените каждую дату сообщения на дату следующего дня.");
+            Console.WriteLine();
+
+            Console.WriteLine("введите дату в формате дата.месяц.год(например 25.11.2020)");
+            string text = (Console.ReadLine());
+
+            string pattern = "[0-3][0-9].[0-1][0-9].[1,2][9,0][0-9][0-9]";
+
+            MatchCollection matches;
+            Regex reg = new Regex(pattern);
+            matches = reg.Matches(text);
+
+            {
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    string updDate = DateTime.Parse(matches[i].Value).AddDays(-1).ToShortDateString();
+                    text = text.Replace(matches[i].Value, updDate);
+                }
+                Console.WriteLine("дата предыдущенго дня {0}", text);
+            }
+            Console.WriteLine("\n3.В сообщении могут" +
+                " содержаться IP - адреса компьютеров в формате d.d.d.d, где d - целое число из диапазона от 0 до 255. " +
+                "\nУдалить из сообщения IP - адреса, в которых последнее число d " +
+                "начинается с заданной цифры\n(данная цифра вводится с клавиатуры");
+            Regex r = new Regex(@"((1\d\d|2([0-4]\d|5[0-5])|\D\d\d?)\.?){4}$");
+            string text1 = "ip = 128.0.0.1";
+            string text2 = "ip = 256.0.0.1";
+            string text3 = "ip = 0.0.0.0";
+            string text4 = "ip = 255.255.255.255";
+            string text5 = "ip = 255:228:0:0";
+
+            Console.WriteLine(r.IsMatch(text1) ? text1 + " - Правильный IP" : text1 + " - Неправильный IP");
+            Console.WriteLine(r.IsMatch(text2) ? text2 + " - Правильный IP" : text2 + " - Неправильный IP");
+            Console.WriteLine(r.IsMatch(text3) ? text3 + " - Правильный IP" : text3 + " - Неправильный IP");
+            Console.WriteLine(r.IsMatch(text4) ? text4 + " - Правильный IP" : text4 + " - Неправильный IP");
+            Console.WriteLine(r.IsMatch(text5) ? text5 + " - Правильный IP" : text5 + " - Неправильный IP");
+            Console.WriteLine("Для завершения нажмите на любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Регулярки/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Регулярки
+{
+    class Program
+    {
+        public static void Print(string str) => Console.WriteLine(str);
+        public static void RegexPart1()
+        {
+            Print($"Задания. Запишите регулярное выражение, соответствующее:" +
+                "\n 1.дате в формате дд.мм.гг или дд.мм.гггг" +
+                "\n 2.времени в формате чч.мм или чч: мм" + "" +
+                " \n 3.целому числу(со знаком и без)" + " \n 4.вещественному числу(со знаком и без, с дробной частью и без, " +
+                " с целой частью и без)");
+            Console.WriteLine();
+            Print("Первое задание:");
+            Regex dateformat = new Regex(@"\d{2}\.\d{2}\.\d{4}");
+            string date1 = "24.11.2020";
+            string date2 = "24 ноября 2020 года";
+            string date3 = "Срок сдачи задания: 25.11.2020";
+
+            Console.WriteLine($"Первая дата: {date1}:{dateformat.IsMatch(date1)}");
+            Console.WriteLine($"Вторая дата: {date2}:{dateformat.IsMatch(date2)}");
+            Console.WriteLine($"Третья дата: {date3}:{dateformat.IsMatch(date3)}");
+            Console.WriteLine();
+
+            Print("Второе задание:");
+            Regex timeformat = new Regex(@"\d{2}\:\d{2}");
+            string time1 = "21:24";
+            string time2 = "25 минут десятого";
+            string time3 = "Сейчас 21:24";
+            Console.WriteLine($"Первое время: {time1}:{timeformat.IsMatch(time1)}");
+            Console.WriteLine($"Второе время: {time2}:{timeformat.IsMatch(time2)}");
+            Console.WriteLine($"Третье время: {time3}:{timeformat.IsMatch(time3)}");
+            Console.WriteLine();
+
+            Print("Третье задание:");
+            Regex intnumber = new Regex(@"^[+-]?\d+$");
+            string intnumber1 = "4.43";
+            string intnumber2 = "-58";
+            string intnumber3 = "4528";
+            Console.WriteLine($"Первое число: {intnumber1}:{intnumber.IsMatch(intnumber1)}");
+            Console.WriteLine($"Второе число: {intnumber2}:{intnumber.IsMatch(intnumber2)}");
+            Console.WriteLine($"Третье число: {intnumber3}:{intnumber.IsMatch(intnumber3)}");
+            Console.WriteLine();
+
+            Print("Четвертое задание:");
+            Regex decimalnumber = new Regex(@"^[+-]?\d+(\.\d+)?$");
+            string decimalnumber1 = "4.43";
+            string decimalnumber2 = "-129.925";
+            string decimalnumber3 = "сорок пять целых и 15 десятых";
+            Console.WriteLine($"Первое число: {decimalnumber1}:{decimalnumber.IsMatch(decimalnumber1)}");
+            Console.WriteLine($"Второе число: {decimalnumber2}:{decimalnumber.IsMatch(decimalnumber2)}");
+            Console.WriteLine($"Третье число: {decimalnumber3}:{decimalnumber.IsMatch(decimalnumber3)}");
+            Console.WriteLine();
+
+        }
+        public static void RegexPart2()
+        {
+            Print("Вторая часть:");
+            Print("Задание. Измените программу так, чтобы на экран дополнительно " +
+                "выводилось количество найденных чисел.");
+            Regex r = new Regex(@"[-+]?\d+");
+            string text = @"5*10=50 -80/40=-2";
+            Match teg = r.Match(text);
+            int sum = 0;
+            int count = 0;// нужно добавить счетчик
+            while (teg.Success)
+            {
+                Console.WriteLine(teg);
+                sum += int.Parse(teg.ToString());
+                teg = teg.NextMatch();
+                count++;// и каждую итерацию его увеличивать на 1
+            }
+            Console.WriteLine("sum=" + sum);
+            Console.WriteLine($"Количество найденных чисел: {count}");
+            Console.WriteLine();
+        }
+        public static void RegexPart3()
+        {
+            Print("Третья часть:");
+            Print("Задание. Измените программу так, чтобы шестизначные номера заменялись " +
+                "на семизначные добавлением 0 \nпосле первых двух цифр. Например," +
+                "номер 12 - 34 - 56 заменился бы на 120 - 34 - 56.");
+            string text = @"Контакты в Москве tel:123-45-67, 123-34-56; fax:123-56-45." +
+                  "Контакты в Саратове tel:12-34-56; fax:11-56-45";
+            Console.WriteLine("Старые данные\n" + text);
+            string newText = Regex.Replace(text, @"\D\d{2}(-\d\d){2}", ":120-34-56");
+
+            Console.WriteLine("Новые данные\n" + newText);
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            RegexPart1();
+            RegexPart2();
+            RegexPart3();
+            Console.WriteLine("Для завершения нажмите на любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
